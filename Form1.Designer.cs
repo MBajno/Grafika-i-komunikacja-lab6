e08c3a0ebf1b1b86742827cc5fef5fa63ec62347
MBajno <79971854+MBajno@users.noreply.github.com>
2021-04-26 23:34:36 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace Projekt_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(32, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(442, 293);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 121);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pobierz obraz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(545, 87);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Wariant 1a";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(545, 128);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Wariant 1b";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(545, 172);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(81, 19);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Wariant 2a";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(545, 217);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(82, 19);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Wariant 2b";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(32, 372);
+            this.trackBar1.Maximum = 127;
+            this.trackBar1.Minimum = -128;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(442, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 13;
+            this.trackBar1.Scroll += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(543, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 27);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Zatwierdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(129, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 26);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Odswiez";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Kontrasty";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_6
+{
+    public partial class Form1 : Form
+    {
+        int height, width;
+        private string odswiez;
+        private int value;
+        private Boolean jest = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+                odswiez = openFileDialog1.FileName;
+
+                pictureBox1.Load(odswiez);
+                pictureBox2.Load(odswiez);
+
+                width = pictureBox1.Image.Width;
+
+                height = pictureBox1.Image.Height;
+
+                jest = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                trackBar1.Maximum = 126;
+                trackBar1.Minimum = 0;
+                wariant_1a();
+            }
+            else if (radioButton2.Checked)
+            {
+                trackBar1.Maximum = -1;
+                trackBar1.Minimum = -128;
+                wariant_1b();
+            }
+            else if (radioButton3.Checked)
+            {
+                trackBar1.Maximum = 127;
+                trackBar1.Minimum = 1;
+                wariant_2a();
+            }
+            else if (radioButton4.Checked)
+            {
+                trackBar1.Maximum = -1;
+                trackBar1.Minimum = -126;
+                wariant_2b();
+            }
+            else { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (jest)
+            {
+                pictureBox1.Load(odswiez);
+            }
+        }
+
+        
+
+        private void wariant_1a()
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            value = trackBar1.Value;
+
+            Color k;
+            int r, g, b;
+
+
+
+            for (int x = 0; x < width; x++)
+
+            {
+                for (int y = 0; y < height; y++)
+
+                {
+
+                    k = b2.GetPixel(x, y);
+                    
+
+                    
+                    r = (127 / (127 - value)) * (k.R - value);
+                    
+                    if(r > 255)
+                    {
+                        r = 255;
+                    }
+                    else if (r < 0)
+                    {
+                        r = 0;
+                    }
+                    
+
+                    g = (127 / (127 - value)) * (k.G - value);
+
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    else if (g < 0)
+                    {
+                        g = 0;
+                    }
+
+                    b = (127 / (127 - value)) * (k.B - value);
+
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
+                    else if (b < 0)
+                    {
+                        b = 0;
+                    }
+
+                    b1.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+
+
+                pictureBox1.Refresh();
+
+            }
+        }
+            
+        
+
+        private void wariant_1b()
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            value = trackBar1.Value;
+
+            Color k;
+            int r, g, b;
+
+
+
+            for (int x = 0; x < width; x++)
+
+            {
+                for (int y = 0; y < height; y++)
+
+                {
+
+                    k = b2.GetPixel(x, y);
+
+                    r = ((127 + value) / 127) * k.R - value;
+
+                    g = ((127 + value) / 127) * k.G - value;
+                    
+                    b = ((127 + value) / 127) * k.B - value;
+
+
+
+                    b1.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+
+
+                pictureBox1.Refresh();
+
+            }
+        }
+
+        private void wariant_2a()
+        { 
+
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            value = trackBar1.Value;
+
+            Color k;
+            int r, g, b;
+
+            
+
+            for (int x = 0; x < width; x++)
+
+            {
+                for (int y = 0; y < height; y++)
+
+                {
+
+                    k = b2.GetPixel(x, y);
+
+
+                    
+                        if (k.R < 127)
+                        {
+                            r = ((127 - value) / 127) * k.R;
+                        }
+                        else if (k.R >= 127)
+                        {
+                            r = ((127 - value) / 127) * k.R + 2 * value;
+                        }
+                        else
+                        {
+                            r = k.R;
+                        }
+
+
+
+                        if (k.G < 127)
+                        {
+                            g = ((127 - value) / 127) * k.G;
+                        }
+                        else if (k.G >= 127)
+                        {
+                            g = ((127 - value) / 127) * k.G + 2 * value;
+                        }
+                        else
+                        {
+                            g = k.G;
+                        }
+
+                        if (k.B < 127)
+                        {
+                            b = ((127 - value) / 127) * k.B;
+                        }
+                        else if (k.B >= 127)
+                        {
+                            b = ((127 - value) / 127) * k.B + 2 * value;
+                        }
+                        else
+                        {
+                            b = k.B;
+                        }
+                    
+                   
+
+
+                    b1.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+
+
+                pictureBox1.Refresh();
+
+            }
+        }
+
+        private void wariant_2b()
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            value = trackBar1.Value;
+
+            Color k;
+            int r, g, b;
+
+
+
+            for (int x = 0; x < width; x++)
+
+            {
+                for (int y = 0; y < height; y++)
+
+                {
+
+                    k = b2.GetPixel(x, y);
+
+                    if (k.R < 127 + value)
+                    {
+                        r = (127 / (127 + value)) * k.R;
+                    }
+                    else if (k.R > 127 - value)
+                    {
+                        r = (127 * k.R + 255 * value) / (127 + value);
+                    }
+                    else
+                    {
+                        r = 127;
+                    }
+
+
+
+                    if (k.G < 127 + value)
+                    {
+                        g = (127 / (127 + value)) * k.G;
+                    }
+                    else if (k.G > 127 - value)
+                    {
+                        g = (127 * k.G + 255 * value) / (127 + value);
+                    }
+                    else
+                    {
+                        g = 127;
+                    }
+
+
+
+                    if (k.B < 127 + value)
+                    {
+                        b = (127 / (127 + value)) * k.B;
+                    }
+                    else if (k.B > 127 - value)
+                    {
+                        b = (127 * k.B + 255 * value) / (127 + value);
+                    }
+                    else
+                    {
+                        b = 127;
+                    }
+
+
+
+
+
+
+
+                    b1.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+
+
+                pictureBox1.Refresh();
+
+            }
+        }
+    }
+}
